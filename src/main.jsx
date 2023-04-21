a7fe1b73e291806dee40d4b8b774672f71c9b104
Daniel Alejandro Cabrera <cabrera_daniel0104@hotmail.com>
2023-04-21 02:11:30 -0300

    fix main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles.css";
 import { HeroesApp } from "./HeroesApp";
 import { BrowserRouter } from "react-router-dom";
 
